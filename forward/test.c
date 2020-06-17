0a1866f217e60fc86cee60b9610197ed969027c6
xiehuan <2429753441@qq.com>
2020-06-17 16:06:02 +0800

    Update test.c

--- forward/test.c
+++ forward/test.c
@@ -2,3 +2,4 @@ test
 libopenssl0_9_8-0.9.8j-0.74.1.11763.0.PTF.991722.x86_64.rpm
 
 https://www.suse.com/zh-cn/support/kb/doc/?id=000018760
+https://unix.stackexchange.com/questions/345483/how-to-set-the-guest-hardware-time-for-qemu-from-libvirt
